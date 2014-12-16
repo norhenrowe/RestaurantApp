2952739e15446726d4c09ab10d5d09bb41b8c1bd
Dtilka <dtilka@gmail.com>
2014-12-15 22:50:29 -0500

    Login Page Layout finished

---
+++ RestauranterAppFinal/RestauranterTabBarController.swift
@@ -0,0 +1,32 @@
+//
+//  RestauranterTabBarController.swift
+//  RestauranterAppFinal
+//
+//  Created by Duncan Tilka on 12/14/14.
+//  Copyright (c) 2014 Duncan Tilka. All rights reserved.
+//
+
+import UIKit
+
+class RestauranterTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+
+
+}
