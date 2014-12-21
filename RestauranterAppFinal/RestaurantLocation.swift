6e52943ddba530dfa38bdb6dbea1d8c2a7bdd1a1
Dtilka <dtilka@gmail.com>
2014-12-20 21:41:16 -0500

    added Delivery ViewController, Delivery Manager view controller

---
+++ RestauranterAppFinal/RestaurantLocation.swift
@@ -0,0 +1,13 @@
+//
+//  RestaurantLocation.swift
+//  RestauranterAppFinal
+//
+//  Created by Duncan Tilka on 12/20/14.
+//  Copyright (c) 2014 Duncan Tilka. All rights reserved.
+//
+
+import UIKit
+
+class RestaurantLocation: UIViewControllerl {
+   
+}
