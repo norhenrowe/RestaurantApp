9e3d32dd6c63c00095d6c9877ffbf893bc4b4d69
Dtilka <dtilka@gmail.com>
2015-01-06 15:57:18 -0500

    Changes made to tracker button View, Slide for waiter button examples made,Waiter views containers made, bunch of other little changes, Location Base example started

---
+++ RestauranterAppFinal/LocationBasePageAfterLogin.swift
@@ -0,0 +1,35 @@
+//
+//  LocationBasePageAfterLogin.swift
+//  RestauranterAppFinal
+//
+//  Created by Duncan Tilka on 1/6/15.
+//  Copyright (c) 2015 Duncan Tilka. All rights reserved.
+//
+
+import UIKit
+
+class LocationBasePageAfterLogin: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ RestauranterAppFinal/MyOrderHistoryContainer-Tracker.swift
@@ -0,0 +1,35 @@
+//
+//  MyOrderHistoryContainer-Tracker.swift
+//  RestauranterAppFinal
+//
+//  Created by Duncan Tilka on 1/6/15.
+//  Copyright (c) 2015 Duncan Tilka. All rights reserved.
+//
+
+import UIKit
+
+class MyOrderHistoryContainer_Tracker: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
